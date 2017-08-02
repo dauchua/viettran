e6fda81cdc4c60e9e8d4d07e8c92efb592458518
viettranquoc <tqviet.hcmus@gmail.com>
2017-08-02 10:13:43 +0700

    Update install_pagespeed.sh

--- install_pagespeed.sh
+++ install_pagespeed.sh
@@ -1,19 +1,19 @@
- yum -y install gcc-c++ pcre-devel pcre-devel zlib-devel make unzip openssl-devel wget
- mkdir -p /opt/nginx/modules
+yum -y install gcc-c++ pcre-devel pcre-devel zlib-devel make unzip openssl-devel wget
+mkdir -p /opt/nginx/modules
  
- cd /opt/nginx/modules
- wget https://github.com/pagespeed/ngx_pagespeed/archive/release-1.9.32.1-beta.zip
- unzip release-1.9.32.1-beta.zip
+cd /opt/nginx/modules
+wget https://github.com/pagespeed/ngx_pagespeed/archive/release-1.9.32.1-beta.zip
+unzip release-1.9.32.1-beta.zip
  
- cd ngx_pagespeed-release-1.9.32.1-beta/
- wget https://dl.google.com/dl/page-speed/psol/1.9.32.1.tar.gz
- tar -xzf 1.9.32.1.tar.gz
+cd ngx_pagespeed-release-1.9.32.1-beta/
+wget https://dl.google.com/dl/page-speed/psol/1.9.32.1.tar.gz
+tar -xzf 1.9.32.1.tar.gz
 
- cd /opt/nginx/
- wget http://nginx.org/download/nginx-1.7.6.tar.gz
- tar -zxf nginx-1.7.6.tar.gz
- cd nginx-1.7.6/
- ./configure --add-module=/opt/nginx/modules/ngx_pagespeed-release-1.9.32.1-beta \
+cd /opt/nginx/
+wget http://nginx.org/download/nginx-1.7.6.tar.gz
+tar -zxf nginx-1.7.6.tar.gz
+cd nginx-1.7.6/
+./configure --add-module=/opt/nginx/modules/ngx_pagespeed-release-1.9.32.1-beta \
 --prefix=/usr/local/nginx \
 --sbin-path=/usr/local/sbin/nginx \
 --conf-path=/etc/nginx/nginx.conf \
